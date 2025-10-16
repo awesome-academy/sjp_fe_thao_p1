4bd6a904e63a5d37baf1b2a914535430c1c176dd
Ho Sy Thao <hosithao1622004@gmail.com>
2025-10-16 21:27:28 +0700

    feat: Tích hợp API cho chức năng Đăng ký

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,14 @@ import './i18n';
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { Toaster } from 'react-hot-toast';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <Toaster position="top-right" reverseOrder={false} />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
